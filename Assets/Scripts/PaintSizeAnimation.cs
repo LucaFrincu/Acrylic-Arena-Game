a60e2a65cfc46c5d830baf8a9030bb10693109e7
Donnna33 <167973405+Donnna33@users.noreply.github.com>
2024-05-20 11:23:00 +0300

    Animatii buton+ cutscene

---
+++ Assets/Scripts/PaintSizeAnimation.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor.UI;
+
+public class PaintSizeAnimation : MonoBehaviour
+{
+    private Vector3 originalScale;
+
+    void Start()
+    {
+        originalScale = transform.localScale;
+    }
+
+    public void Increase()
+    {
+        LeanTween.scale(gameObject, originalScale * 1.2f, 0.2f).setOnComplete(() =>
+        {
+            LeanTween.scale(gameObject, originalScale, 0.2f);
+        });
+    }
+
+    public void Decrease()
+    {
+        LeanTween.scale(gameObject, originalScale * 0.8f, 0.2f).setOnComplete(() =>
+        {
+            LeanTween.scale(gameObject, originalScale, 0.2f);
+        });
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Increase();
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Decrease();
+        }
+    }
+}
